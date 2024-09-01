2894e73db90199e0645ec7be5fd5bdb15d1f0ca4
markdackus247 <79475320+markdackus247@users.noreply.github.com>
2024-09-01 20:31:15 +0200

    Update home.php

--- views/home.php
+++ views/home.php
@@ -10,16 +10,16 @@
 
     <div class="mt-6 mb-16 w-11/12 p-6 space-y-8 sm:p-8 bg-white mx-auto">
 
-        <h2 class="text-2xl font-bold dark:text-white">Automatisering voor en door studenten</h2>
+        <h2 class="text-2xl font-bold ">Automatisering voor en door studenten</h2>
         <p class="my-4 font-bold text-gray-700">Het project MBO digitaal is een project van de Software Development
             opleiding van het Vista College. Studenten helpen om allerdaagse zaken zoals keuzedelen, beoordelingen en
             examenroosters overzichtelijk te maken voor studenten. Dit doen ze door processen, die veelal door docenten
             worden uitgevoerd, te automatiseren.</p>
-        <p class="mb-4 font-normal text-gray-700 dark:text-gray-500">Dit project is een onderdeel van Challenge Based
+        <p class="mb-4 font-normal text-gray-700 ">Dit project is een onderdeel van Challenge Based
             Learning (CBL) waarin groepen studenten worden uitgedaagd om het bestaande project MBO Digitaal uit te breiden met
             nieuwe functionaliteiten die van meerwaarde zijn voor studenten en docenten.</p>
 
-        <h3 class="text-xl font-bold dark:text-white">Overzicht van onze opleidingen</h3>
+        <h3 class="text-xl font-bold ">Overzicht van onze opleidingen</h3>
 
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
